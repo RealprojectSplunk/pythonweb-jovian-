d0ecbed63590c5d1c837737c002504e1bbab9f17
RealprojectSplunk <156848223+RealprojectSplunk@users.noreply.github.com>
2024-01-22 18:53:44 +0000

    new updates

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 @app.route("/")
-def hello():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("home.html")
 
 
-if 1==1:
+if __name__=="__main__":
   print("i am inside you")
   app.run(host='0.0.0.0',debug=True)
