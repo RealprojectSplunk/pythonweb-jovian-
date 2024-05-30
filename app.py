fab5a3352ca290e3d51c676ab99c3416cf515574
RealprojectSplunk <156848223+RealprojectSplunk@users.noreply.github.com>
2024-05-30 05:07:51 +0000

    New changes

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ from flask import Flask, render_template, request, redirect
 app=Flask(__name__)
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", jobs=JOBS, Company="Parayil"  )
 
+JOBS= [
+  {"id":1,"title":"Data Analyst","location":"Bengaluru, India","salary":"100K"},
+  {"id":2,"title":"Data Scientist","location":"Delhi, India","salary":"200k"}
+   
+]
 
 if __name__=="__main__":
   print("i am inside you")
